7d378fc5a13649f9ba95ce5811eafa33b7218151
BuildTools <milsonneau.jean@free.fr>
2016-10-11 13:43:16 +0200

    added camera to tank

--- Cars/camera01.cs
+++ Cars/camera01.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class camera01 : MonoBehaviour {
 
+	public float height = 2;
+	public float distance = 5;
+
 	private Vector3 _velocity = Vector3.zero;
 	private Rigidbody _parentBody;
 
@@ -18,9 +21,9 @@ public class camera01 : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		Vector3 target = transform.parent.position + transform.parent.up * 2 - transform.parent.GetComponent<Rigidbody> ().velocity.normalized * 5;
+		Vector3 target = transform.parent.position + transform.parent.up * height - transform.parent.GetComponent<Rigidbody> ().velocity.normalized * distance;
 		if (_parentBody.velocity.magnitude < 0.01 && _parentBody.velocity.magnitude > -0.01) {
-			target = transform.parent.position + transform.parent.up * 2 - transform.parent.forward * 5;
+			target = transform.parent.position + transform.parent.up * height - transform.parent.forward * distance;
 		}
 		transform.position = Vector3.SmoothDamp(transform.position, target, ref _velocity, 0.1f);
 		transform.LookAt (transform.parent);
