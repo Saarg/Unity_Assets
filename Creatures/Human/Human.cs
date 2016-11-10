b7c959a5f5eb4209a8fe0e298626d80cae7ab4e0
BuildTools <milsonneau.jean@free.fr>
2016-11-10 15:35:01 +0100

    adding rigged human

--- Creatures/Human/Human.cs
+++ Creatures/Human/Human.cs
@@ -5,28 +5,16 @@ public class Human : MonoBehaviour {
 
   private MultiOSControls _controls;
 
-  private Rigidbody _Head;
-  private Rigidbody _Body;
-  private Rigidbody _LeftHips, _LeftKnee, _LeftAnkle;
-  private Rigidbody _RightHips, _RightKnee, _RightAnkle;
-  private Rigidbody _LeftArm, _LeftElbow;
-  private Rigidbody _RightArm, _RightElbow;
+  public Rigidbody _Head;
+  public Rigidbody _Pelvis;
+  public Rigidbody _Spine;
+  public Rigidbody _LeftHips, _LeftKnee, _LeftAnkle;
+  public Rigidbody _RightHips, _RightKnee, _RightAnkle;
+  public Rigidbody _LeftArm, _LeftElbow;
+  public Rigidbody _RightArm, _RightElbow;
 
   void Awake () {
 		_controls = GameObject.Find ("Scripts").GetComponent<MultiOSControls> ();
-
-    _Head = transform.Find("Head").gameObject.GetComponent<Rigidbody>();
-    _Body = transform.Find("Body").gameObject.GetComponent<Rigidbody>();
-    _LeftHips = transform.Find("Left Hips").gameObject.GetComponent<Rigidbody>();
-    _LeftKnee = transform.Find("Left Knee").gameObject.GetComponent<Rigidbody>();
-    _LeftAnkle = transform.Find("Left Ankle").gameObject.GetComponent<Rigidbody>();
-    _RightHips = transform.Find("Right Hips").gameObject.GetComponent<Rigidbody>();
-    _RightKnee = transform.Find("Right Knee").gameObject.GetComponent<Rigidbody>();
-    _RightAnkle = transform.Find("Right Ankle").gameObject.GetComponent<Rigidbody>();
-    _LeftArm = transform.Find("Left Arm").gameObject.GetComponent<Rigidbody>();
-    _LeftElbow = transform.Find("Left Elbow").gameObject.GetComponent<Rigidbody>();
-    _RightArm = transform.Find("Right Arm").gameObject.GetComponent<Rigidbody>();
-    _RightElbow = transform.Find("Right Elbow").gameObject.GetComponent<Rigidbody>();
 	}
 
 	// Use this for initialization
@@ -36,7 +24,7 @@ public class Human : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-    _Body.AddForce(transform.forward * -30 * _controls.getValue("Forward"));
+    //_Body.AddForce(transform.forward * -30 * _controls.getValue("Forward"));
     //_Body.AddForce(transform.up * 630);
 	}
 }
