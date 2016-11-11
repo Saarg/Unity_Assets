2576b2ac673eaf1822371cbbf730532c88e358af
BuildTools <milsonneau.jean@free.fr>
2016-11-11 22:27:22 +0100

    proof of concept animation needs to be redonne

---
+++ Creatures/Human/Human.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Human : MonoBehaviour {
+
+  private Rigidbody _Pelvis;
+  public Rigidbody _Spine;
+
+	// Use this for initialization
+	void Start () {
+    _Pelvis = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  void OnTriggerEnter	(Collider other) {
+    if (other.name == "Terrain") {
+      _Pelvis.constraints  |= RigidbodyConstraints.FreezePositionY;
+      _Spine.constraints  |= _Pelvis.constraints;
+    }
+  }
+}
