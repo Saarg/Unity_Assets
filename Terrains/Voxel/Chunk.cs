cdca8846eef594332b3c8ba313028a6545c0dc1b
BuildTools <milsonneau.jean@free.fr>
2016-12-15 15:54:33 +0100

    voxel tutorial pt3

--- Terrains/Voxel/Chunk.cs
+++ Terrains/Voxel/Chunk.cs
@@ -6,7 +6,11 @@ using System.Collections;
 [RequireComponent(typeof(MeshCollider))]
 
 public class Chunk : MonoBehaviour {
-  Block[ , , ] blocks;
+  private Block[ , , ] blocks = new Block[chunkSize, chunkSize, chunkSize];
+
+  public World world;
+  public WorldPos pos;
+
   public static int chunkSize = 16;
   public bool update = true;
 
@@ -16,30 +20,42 @@ public class Chunk : MonoBehaviour {
   void Start () {
     filter = gameObject.GetComponent<MeshFilter>();
     coll = gameObject.GetComponent<MeshCollider>();
-    //past here is just to set up an example chunk
-    blocks = new Block[chunkSize, chunkSize, chunkSize];
-    for (int x = 0; x < chunkSize; x++)
-    {
-      for (int y = 0; y < chunkSize; y++)
-      {
-        for (int z = 0; z < chunkSize; z++)
-        {
-          blocks[x, y, z] = new BlockAir();
-        }
-      }
-    }
-    blocks[3, 5, 2] = new Block();
-    blocks[4, 5, 2] = new BlockGrass();
-    UpdateChunk();
   }
 
   //Update is called once per frame
   void Update () {
+    if (update)
+    {
+      update = false;
+      UpdateChunk();
+    }
   }
 
   public Block GetBlock(int x, int y, int z)
   {
+    if(InRange(x) && InRange(y) && InRange(z))
     return blocks[x, y, z];
+    return world.GetBlock(pos.x + x, pos.y + y, pos.z + z);
+  }
+
+  public void SetBlock(int x, int y, int z, Block block)
+  {
+    if (InRange(x) && InRange(y) && InRange(z))
+    {
+      blocks[x, y, z] = block;
+    }
+    else
+    {
+      world.SetBlock(pos.x + x, pos.y + y, pos.z + z, block);
+    }
+  }
+
+  public static bool InRange(int index)
+  {
+    if (index < 0 || index >= chunkSize)
+    return false;
+
+    return true;
   }
 
   //Updates the chunk based on its contents
