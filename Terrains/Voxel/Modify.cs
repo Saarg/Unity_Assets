26ea5f57a3a45c1b0ddb821385d3de353269863d
BuildTools <milsonneau.jean@free.fr>
2016-12-16 22:09:07 +0100

    updated basic player control in voxel demo

--- Terrains/Voxel/Modify.cs
+++ Terrains/Voxel/Modify.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class Modify : MonoBehaviour
 {
+  private MultiOSControls _controls;
 
-  Vector2 rot;
+  private Vector2 rot;
 
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
+  void Start() {
+    _controls = GameObject.Find ("Scripts").GetComponent<MultiOSControls> ();
+  }
+
+  void Update() {
+    if (_controls.getValue("Destroy1") != 0.0f)
     {
       RaycastHit hit;
       if (Physics.Raycast(transform.position, transform.forward,out hit, 100 ))
@@ -18,15 +22,14 @@ public class Modify : MonoBehaviour
     }
 
     rot= new Vector2(
-      rot.x + Input.GetAxis("mouse x")/3,
-      rot.y + Input.GetAxis("mouse y")/3);
+      rot.x + _controls.getValue("Horizontal1")*3,
+      rot.y + _controls.getValue("Vertical1")*2
+    );
 
-    transform.localRotation = Quaternion.AngleAxis(rot.x, Vector3.up);
-    transform.localRotation *= Quaternion.AngleAxis(rot.y, Vector3.left);
+    transform.parent.localRotation = Quaternion.AngleAxis(rot.x, Vector3.up);
+    transform.localRotation = Quaternion.AngleAxis(rot.y, Vector3.left);
 
-    transform.position += transform.forward * (Input.GetKey("z") ? 1 : 0);
-    transform.position += transform.right * (Input.GetKey("q") ? 1 : 0);
-    transform.position += transform.forward * (Input.GetKey("s") ? -1 : 0);
-    transform.position += transform.right * (Input.GetKey("d") ? -1 : 0);
+    transform.parent.position += transform.parent.forward * -_controls.getValue("Forward1")/3;
+    transform.parent.position += transform.parent.right * _controls.getValue("Sideway1")/3;
   }
 }
