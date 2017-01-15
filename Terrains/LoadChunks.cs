74802791b74b3006c95245a33f8a7d0666a265dd
BuildTools <milsonneau.jean@free.fr>
2017-01-15 22:05:30 +0100

    still trying to improve loading

--- Terrains/LoadChunks.cs
+++ Terrains/LoadChunks.cs
@@ -1,68 +1,88 @@
 using UnityEngine;
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
+
 public class LoadChunks : MonoBehaviour {
   public World world;
   public int _renderDistance = 256;
-  public int _loadingDistance = 6;
-  public int _buildSpeed = 64;
+  public int _buildSpeed = 4;
 
   int timer = 0;
 
   List<WorldPos> updateList = new List<WorldPos>();
   List<WorldPos> buildList = new List<WorldPos>();
 
+  private WorldPos playerPos;
+
+  private Thread _chunkWorker;
+  protected bool _continueTh = true;
+
+  void Start() {
+    _chunkWorker = new Thread(new ThreadStart(FindChunksToLoad));
+    _chunkWorker.Start();
+  }
+
+  void OnDestroy() {
+    _continueTh = false;
+  }
+
   void Update () {
+    //Get the position of this gameobject to generate around
+    playerPos = new WorldPos(
+      Mathf.FloorToInt(transform.position.x / Chunk.chunkSize) * Chunk.chunkSize,
+      Mathf.FloorToInt(transform.position.y / Chunk.chunkSize) * Chunk.chunkSize,
+      Mathf.FloorToInt(transform.position.z / Chunk.chunkSize) * Chunk.chunkSize
+    );
+
     if (DeleteChunks())
       return;
-    FindChunksToLoad();
+    // FindChunksToLoad();
     LoadAndRenderChunks();
   }
 
   void FindChunksToLoad()
   {
-    //Get the position of this gameobject to generate around
-    WorldPos playerPos = new WorldPos(
-      Mathf.FloorToInt(transform.position.x / Chunk.chunkSize) * Chunk.chunkSize,
-      Mathf.FloorToInt(transform.position.y / Chunk.chunkSize) * Chunk.chunkSize,
-      Mathf.FloorToInt(transform.position.z / Chunk.chunkSize) * Chunk.chunkSize
-    );
-    //If there aren't already chunks to generate
-    if (updateList.Count == 0)
-    {
-      int added = 0;
-      //Cycle through the array of positions
-      for (int i = 0; i < chunkPositions.Length && i < _loadingDistance*_loadingDistance; i++)
+    Debug.Log("Starting Chunk thread");
+    while(_continueTh) {
+      //If there aren't already chunks to generate
+      if (updateList.Count == 0)
       {
-        //translate the player position and array position into chunk position
-        WorldPos newChunkPos = new WorldPos(
-          chunkPositions[i].x * Chunk.chunkSize + playerPos.x,
-          chunkPositions[i].y * Chunk.chunkSize + playerPos.y - Chunk.chunkSize,
-          chunkPositions[i].z * Chunk.chunkSize + playerPos.z
-        );
-
-        //load a column of chunks in this position
-        for (int y = newChunkPos.y - Chunk.chunkSize; y <= newChunkPos.y + 2*Chunk.chunkSize; y += Chunk.chunkSize)
+        int added = 0;
+        //Cycle through the array of positions
+        for (int i = 0; i < chunkPositions.Length; i++)
         {
-          WorldPos tmpPos = new WorldPos(newChunkPos.x, y, newChunkPos.z);
+          //translate the player position and array position into chunk position
+          WorldPos newChunkPos = new WorldPos(
+            chunkPositions[i].x * Chunk.chunkSize + playerPos.x,
+            chunkPositions[i].y * Chunk.chunkSize + playerPos.y - Chunk.chunkSize,
+            chunkPositions[i].z * Chunk.chunkSize + playerPos.z
+          );
 
-          Chunk newChunk = world.GetChunk(tmpPos.x, tmpPos.y, tmpPos.z);
-          //If the chunk already exists and it's already
-          //rendered or in queue to be rendered continue
-          if (newChunk != null && (newChunk.rendered || updateList.Contains(newChunkPos))) {
-            continue;
+          //load a column of chunks in this position
+          for (int y = newChunkPos.y - Chunk.chunkSize; y <= newChunkPos.y + 2*Chunk.chunkSize; y += Chunk.chunkSize)
+          {
+            WorldPos tmpPos = new WorldPos(newChunkPos.x, y, newChunkPos.z);
+
+            Chunk newChunk = world.GetChunk(tmpPos.x, tmpPos.y, tmpPos.z);
+            //If the chunk already exists and it's already
+            //rendered or in queue to be rendered continue
+            if (newChunk != null && (newChunk.rendered || updateList.Contains(newChunkPos))) {
+              continue;
+            }
+
+            buildList.Add(tmpPos);
+            updateList.Add(tmpPos);
+            added++;
           }
 
-          buildList.Add(tmpPos);
-          updateList.Add(tmpPos);
-          added++;
-        }
-
-        if(added > _buildSpeed) {
-          return;
+          if(added > _buildSpeed) {
+            break;
+          }
         }
       }
     }
+    Debug.Log("Ending Chunk thread");
   }
 
   void BuildChunk(WorldPos pos)
@@ -73,9 +93,10 @@ public class LoadChunks : MonoBehaviour {
 
   void LoadAndRenderChunks()
   {
+    float startTime = Time.realtimeSinceStartup;
     if (buildList.Count != 0)
     {
-      for (int i = 0; i < buildList.Count && i < _buildSpeed; i++)
+      for (int i = 0; i < buildList.Count && (Time.realtimeSinceStartup - startTime) < 0.01f; i++)
       {
         BuildChunk(buildList[0]);
         buildList.RemoveAt(0);
@@ -114,7 +135,8 @@ public class LoadChunks : MonoBehaviour {
     return false;    //Add this line
   }
 
-  static  WorldPos[] chunkPositions= {   new WorldPos( 0, 0,  0), new WorldPos(-1, 0,  0), new WorldPos( 0, 0, -1), new WorldPos( 0, 0,  1), new WorldPos( 1, 0,  0),
+  static  WorldPos[] chunkPositions= {
+    new WorldPos( 0, 0,  0), new WorldPos(-1, 0,  0), new WorldPos( 0, 0, -1), new WorldPos( 0, 0,  1), new WorldPos( 1, 0,  0),
     new WorldPos(-1, 0, -1), new WorldPos(-1, 0,  1), new WorldPos( 1, 0, -1), new WorldPos( 1, 0,  1), new WorldPos(-2, 0,  0),
     new WorldPos( 0, 0, -2), new WorldPos( 0, 0,  2), new WorldPos( 2, 0,  0), new WorldPos(-2, 0, -1), new WorldPos(-2, 0,  1),
     new WorldPos(-1, 0, -2), new WorldPos(-1, 0,  2), new WorldPos( 1, 0, -2), new WorldPos( 1, 0,  2), new WorldPos( 2, 0, -1),
@@ -152,5 +174,6 @@ public class LoadChunks : MonoBehaviour {
     new WorldPos( 7, 0, -2), new WorldPos( 7, 0,  2), new WorldPos(-7, 0, -3), new WorldPos(-7, 0,  3), new WorldPos(-3, 0, -7),
     new WorldPos(-3, 0,  7), new WorldPos( 3, 0, -7), new WorldPos( 3, 0,  7), new WorldPos( 7, 0, -3), new WorldPos( 7, 0,  3),
     new WorldPos(-6, 0, -5), new WorldPos(-6, 0,  5), new WorldPos(-5, 0, -6), new WorldPos(-5, 0,  6), new WorldPos( 5, 0, -6),
-    new WorldPos( 5, 0,  6), new WorldPos( 6, 0, -5), new WorldPos( 6, 0,  5) };
+    new WorldPos( 5, 0,  6), new WorldPos( 6, 0, -5), new WorldPos( 6, 0,  5)
+  };
   }
